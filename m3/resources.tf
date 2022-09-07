b145684d2a16c162af8b7fc3b34d141e0dd068c2
Levi <levimcdonough@gmail.com>
2022-09-07 17:44:41 -0400

    adding m4 and m5, also added a base github actions workflow.

--- m3/resources.tf
+++ m3/resources.tf
@@ -35,17 +35,14 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~>2.0"
 
-  name = "vividly"
+  name = "vividly-primary"
 
   cidr            = var.cidr_block
   azs             = slice(data.aws_availability_zones.available.names, 0, var.subnet_count)
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
-
   enable_nat_gateway = true
-
   create_database_subnet_group = true
-
 
   tags = {
     Environment = "Development"
